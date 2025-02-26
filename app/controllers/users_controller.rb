df99c5a18b40a84b3f3f393ce77a1f170eccb1b0
hendrik <li.zhiqiang2011@gmail.com>
2025-02-26 18:38:22 +0700

    handle error user not found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,11 @@ class UsersController < ApplicationController
     private
 
     def set_user
-        @user = User.find(params[:id])
+        @user = User.find_by(id: params[:id])
+        unless @user
+            render_json("User not found!", 404, "error")
+            return
+        end
     end
 
     def user_params
