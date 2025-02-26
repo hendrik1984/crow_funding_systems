77ef41884060f13156f58bc699ad056b2f05ffdc
hendrik <li.zhiqiang2011@gmail.com>
2025-02-26 21:02:10 +0700

    update response user data to return token only

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     def login
         @user = User.find_by(email: params[:email])
         if @user&.authenticate(params[:password])
-            render_json("Login Successfully", 200, "success", { user_id: @user.id, email: user.email })
+            render_json("Login Successfully", 200, "success", { token: @user.token })
         else
             render_json("Invalid email or password", 401, "error")
         end
